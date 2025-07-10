d783403855efc57dfac66db70f42c44bea6f6ea7
unknown <gasparrmiranda449@gmail.com>
2025-07-10 17:26:38 -0300

    Eliminados archivos de Firebase Hosting

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -1,9 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider,signInWithPopup} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
