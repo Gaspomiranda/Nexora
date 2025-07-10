e675987cb930485e5891578a1e5fa4a46412fd63
unknown <gasparrmiranda449@gmail.com>
2025-07-10 15:17:41 -0300

    Fix: eliminado import incorrecto de globals.css en pryvacy.js

--- frontend/pages/privacy.js
+++ frontend/pages/privacy.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
-import '../styles/globals.css';
+
 
 export default function PrivacyPolicy() {
   return (
